7ec00c005e4b94096e895a53742a83a23450b0e2
Misieq01 <38589417+Misieq01@users.noreply.github.com>
2022-11-03 15:31:46 +0100

    Remove trash after merges

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,14 +1,7 @@
 import { ethers } from 'ethers'
 import { hexlify } from 'ethers/lib/utils'
 import { OpcodesNamesArray } from './opcodes'
-import {
-    ICallTypeTraceLogs,
-    TReturnedTraceLogs,
-    IStructLog,
-    IStructLogWithIndex,
-    ICreateTypeTraceLogs,
-    IReturnTypeTraceLogs,
-} from './types'
+import { ICallTypeTraceLogs, TReturnedTraceLogs, IStructLog, IStructLogWithIndex, ICreateTypeTraceLogs } from './types'
 import { writeFileSync, mkdirSync } from 'fs'
 
 export const filterForBaseLogs = (structLogs: IStructLog[]) => {
