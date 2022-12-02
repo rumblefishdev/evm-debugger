68267d255e3233ebe6d9b059133639a31747f980
Marek Kowalski <kowalski0123@gmail.com>
2022-12-02 12:39:30 +0100

    Add MVP working browser-based disassembler

---
+++ packages/disassembler-minimal-frontend/src/disassembler.js
@@ -0,0 +1,26 @@
+export class Disassembler {
+  constructor() {
+    this.initialized = false;
+  }
+
+  async _initialize() {
+    if (!this.initialized) {
+      console.log('Initializing python environment');
+      this.pyodide = await loadPyodide({});
+      await this.pyodide.loadPackage('micropip')
+      const pip = this.pyodide.pyimport('micropip')
+      await pip.install('pyevmasm')
+      this.initialized = true;
+    }
+  }
+
+  async disassemble(code) {
+    await this._initialize();
+    const textResult = await this.pyodide.runPythonAsync(`
+      import pyevmasm, json, binascii
+      res = pyevmasm.disassemble_all(binascii.unhexlify("${code}"))
+      json.dumps([x.__dict__ for x in res])
+    `);
+    return JSON.parse(textResult);
+  }
+}
