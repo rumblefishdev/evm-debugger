19fb2cfe84a2937e957b0c93605bb055f80341f8
Misieq01 <38589417+Misieq01@users.noreply.github.com>
2022-12-09 14:31:36 +0100

    Update script.ts

--- packages/analyzer/script.ts
+++ packages/analyzer/script.ts
@@ -1,6 +1,5 @@
 import { network } from 'hardhat'
 import type { TTransactionData } from '@evm-debuger/types'
-import { writeFileSync } from 'fs'
 
 import { TxAnalyzer } from './src/txAnalyzer'
 
@@ -9,8 +8,6 @@ const TRANSACTION_HASH = '0x4c39f85ff29a71b49d4237fe70d68366ccd28725e1343500c120
 // eslint-disable-next-line import/newline-after-import
 ;(async () => {
   const traceResult = await network.provider.send('debug_traceTransaction', [TRANSACTION_HASH, { tracer: 'callTracer' }])
-
-  writeFileSync('trace.json', JSON.stringify(traceResult, null, 2))
 
   const transactionInfo = await network.provider.send('eth_getTransactionByHash', [TRANSACTION_HASH])
 
@@ -21,7 +18,7 @@ const TRANSACTION_HASH = '0x4c39f85ff29a71b49d4237fe70d68366ccd28725e1343500c120
 
   const analyzer = new TxAnalyzer(transactionData)
 
-  const result = await analyzer.analyze()
+  const result = analyzer.analyze()
 
-  // console.log(result)
+  console.log(result)
 })()
