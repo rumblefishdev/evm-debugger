8a56577b8ccab8873a0eeb94f8e24a6b13f3a2f3
Misieq01 <38589417+Misieq01@users.noreply.github.com>
2022-12-22 01:22:07 +0100

    Remove unused ref

--- packages/frontend/src/components/StructlogAcordionPanel/StructlogAcordionPanel.tsx
+++ packages/frontend/src/components/StructlogAcordionPanel/StructlogAcordionPanel.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState, useRef, useEffect } from 'react'
+import React, { useCallback, useState } from 'react'
 import { AccordionDetails } from '@mui/material'
 
 import type { StructlogAcordionPanelProps } from './StructlogAcordionPanel.types'
@@ -6,28 +6,18 @@ import { StyledAccordion, StyledAccordionSummary, StyledContainer, StyledWrapper
 
 export const StructlogAcordionPanel = ({ canExpand = false, text, children, ...props }: StructlogAcordionPanelProps) => {
   const [isExpanded, setExpanded] = useState(false)
-  const [containerHeight, setContainerHeight] = useState(0)
-
-  const containerRef = useRef<HTMLDivElement>(null)
-
-  useEffect(() => {
-    const container = containerRef.current
-    if (container) setContainerHeight(container.clientHeight)
-  }, [])
 
   const rootStyles = isExpanded && canExpand ? { flexGrow: 1 } : {}
   const canExpandStyles = canExpand ? { cursor: 'pointer' } : { backgroundColor: 'rgba(0, 0, 0, 0.12)' }
   const handleExpand = useCallback(() => setExpanded(!isExpanded && canExpand), [isExpanded, canExpand])
 
   return (
-    <StyledAccordion ref={containerRef} {...props} expanded={isExpanded && canExpand} sx={{ ...rootStyles, ...canExpandStyles }}>
+    <StyledAccordion {...props} expanded={isExpanded && canExpand} sx={{ ...rootStyles, ...canExpandStyles }}>
       <StyledAccordionSummary onClick={handleExpand}>{text}</StyledAccordionSummary>
       <AccordionDetails>
-        {containerHeight ? (
-          <StyledWrapper>
-            <StyledContainer>{children}</StyledContainer>
-          </StyledWrapper>
-        ) : null}
+        <StyledWrapper>
+          <StyledContainer>{children}</StyledContainer>
+        </StyledWrapper>
       </AccordionDetails>
     </StyledAccordion>
   )
