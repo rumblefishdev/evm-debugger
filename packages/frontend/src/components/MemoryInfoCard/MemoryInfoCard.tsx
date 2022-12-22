86f701a6065d5e651ecd41017270414c9b263868
Misieq01 <38589417+Misieq01@users.noreply.github.com>
2022-12-22 01:17:58 +0100

    Move parsing methods to store selectors

--- packages/frontend/src/components/MemoryInfoCard/MemoryInfoCard.tsx
+++ packages/frontend/src/components/MemoryInfoCard/MemoryInfoCard.tsx
@@ -1,32 +1,21 @@
 import React, { useMemo } from 'react'
 
 import { useTypedSelector } from '../../store/storeHooks'
-import { selectStructlogMemory } from '../../store/structlogs/structlogs.slice'
+import { selectParsedMemory } from '../../store/structlogs/structlogs.slice'
 import { StructlogAcordionPanel } from '../StructlogAcordionPanel'
 
 import type { MemoryInfoCardProps } from './MemoryInfoCard.types'
 import { StyledRecord, StyledRecordIndex, StyledRecordValue, StyledStack } from './styles'
 
 export const MemoryInfoCard = ({ ...props }: MemoryInfoCardProps) => {
-  const memory = useTypedSelector(selectStructlogMemory)
-
-  const parsedMemory = useMemo(() => {
-    return memory.map((memoryItem, index) => {
-      const defaultString = '00000000'
-      const hexValue = (index * 32).toString(16)
-      const paddedHexValue = defaultString.slice(0, Math.max(0, defaultString.length - hexValue.length)) + hexValue
-      const splitMemoryItem = [...memoryItem.match(/.{1,2}/g)]
-
-      return { value: splitMemoryItem, index: paddedHexValue }
-    })
-  }, [memory])
+  const memory = useTypedSelector(selectParsedMemory)
 
   return (
     <StructlogAcordionPanel text="Memory" canExpand={memory.length > 0}>
       <StyledStack {...props}>
-        {parsedMemory.map((memoryItem) => {
+        {memory.map((memoryItem, index) => {
           return (
-            <StyledRecord direction="row">
+            <StyledRecord direction="row" key={index}>
               <StyledRecordIndex>{memoryItem.index}</StyledRecordIndex>
               {memoryItem.value.map((value) => {
                 return <StyledRecordValue>{value}</StyledRecordValue>
