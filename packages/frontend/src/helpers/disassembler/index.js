f078a127e7381288b6820a72087ebd8b2d3a015e
karczu <oskar.karcz@rumblefish.pl>
2022-12-23 12:10:09 +0100

    create disassembler class singleton

--- packages/frontend/src/helpers/disassembler/index.js
+++ packages/frontend/src/helpers/disassembler/index.js
@@ -1,6 +1,25 @@
 import { Disassembler } from './disassembler'
 
-const disassembler = new Disassembler()
-  export async function disassembleBytecode(hexcode) {
+const SingletonDisassembler = (function(){
+  let instance;
+
+  function createDisassembler() {
+    const disassembler = new Disassembler();
+    return disassembler;
+  }
+
+  return {
+    getInstance: function(){
+      if (!instance) {
+        instance = createDisassembler();
+        delete instance.constructor;
+      }
+      return instance;
+    }
+  };
+})();
+
+export async function disassembleBytecode(hexcode) {
+  const disassembler = SingletonDisassembler.getInstance()
   return await disassembler.disassemble(hexcode)
 }
