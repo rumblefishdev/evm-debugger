32167288f9bd8711b2ed98841bb0f7668e078018
kamils <kserafin2806@gmail.com>
2022-12-30 14:59:02 +0100

    infra - revert unnecessary changes

--- packages/analyzer/scripts/script.ts
+++ packages/analyzer/scripts/script.ts
@@ -1,4 +1,4 @@
-const hardhat = require("hardhat")
+import { network } from 'hardhat'
 import type { TTransactionData } from '@evm-debuger/types'
 import { prepareAnalyzer } from './scriptHelper'
 
@@ -6,10 +6,8 @@ const TRANSACTION_HASH = '0x4c39f85ff29a71b49d4237fe70d68366ccd28725e1343500c120
 
 // eslint-disable-next-line import/newline-after-import
 ;(async () => {
-  let newHard = await hardhat.run("node:get-provider");
-
-  const traceResult = await newHard.send('debug_traceTransaction', [TRANSACTION_HASH, { tracer: 'callTracer' }])
-  const transactionInfo = await newHard.send('eth_getTransactionByHash', [TRANSACTION_HASH])
+  const traceResult = await network.provider.send('debug_traceTransaction', [TRANSACTION_HASH, { tracer: 'callTracer' }])
+  const transactionInfo = await network.provider.send('eth_getTransactionByHash', [TRANSACTION_HASH])
 
   const transactionData: TTransactionData = {
     transactionInfo,
@@ -18,6 +16,7 @@ const TRANSACTION_HASH = '0x4c39f85ff29a71b49d4237fe70d68366ccd28725e1343500c120
   }
 
   const analyzer = await prepareAnalyzer(transactionData)
+
   const result = analyzer.analyze()
 
   console.log(result)
