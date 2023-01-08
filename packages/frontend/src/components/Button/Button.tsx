d8b4d81130257ebbaa3f0cc7968ce6480d1a444f
Misieq01 <38589417+Misieq01@users.noreply.github.com>
2023-01-08 22:42:45 +0100

    Update Button.tsx

--- packages/frontend/src/components/Button/Button.tsx
+++ packages/frontend/src/components/Button/Button.tsx
@@ -4,8 +4,6 @@ import type { ButtonProps } from './Button.types'
 import { StyledButton } from './styles'
 
 export const Button = ({ big, ...props }: ButtonProps) => {
-  // TODO: implement "big" as class
-
   return (
     <StyledButton big={big} {...props}>
       {props.children}
