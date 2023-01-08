7daf570ee593a0ddb335866d124888235660602a
Misieq01 <38589417+Misieq01@users.noreply.github.com>
2023-01-08 22:44:10 +0100

    Remove unused type from button

--- packages/frontend/src/components/Button/Button.types.ts
+++ packages/frontend/src/components/Button/Button.types.ts
@@ -2,19 +2,6 @@ import type { ButtonUnstyledProps } from '@mui/base'
 
 export type TButtonVariant = 'text' | 'outlined' | 'contained'
 
-export type TButtonClasses = {
-  root: string
-  text: string
-  outlined: string
-  contained: string
-  disabled: string
-  startIcon: string
-  endIcon: string
-  sizeSmall: string
-  sizeMedium: string
-  sizeLarge: string
-}
-
 export interface ButtonProps extends Omit<ButtonUnstyledProps, 'component'> {
   variant: TButtonVariant
   big?: boolean
