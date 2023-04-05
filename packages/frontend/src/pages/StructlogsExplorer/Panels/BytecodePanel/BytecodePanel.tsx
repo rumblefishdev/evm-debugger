10a5c8feae707b5863d5edf7c1744e199e1720d3
wolszczak96 <wolszczak96@gmail.com>
2023-04-05 16:06:22 +0200

    review changes

--- packages/frontend/src/pages/StructlogsExplorer/Panels/BytecodePanel/BytecodePanel.tsx
+++ packages/frontend/src/pages/StructlogsExplorer/Panels/BytecodePanel/BytecodePanel.tsx
@@ -8,9 +8,10 @@ import { bytecodesSelectors } from '../../../../store/bytecodes/bytecodes.slice'
 import { StyledHeading, StyledListWrapper, StyledSmallPanel } from '../styles'
 import { ExplorerListRow } from '../../../../components/ExplorerListRow'
 import { convertOpcodeToName } from '../../../../helpers/opcodesDictionary'
+import { sourceCodesSelectors } from '../../../../store/sourceCodes/sourceCodes.slice'
 
 import { StyledDisabledBytecode, StyledButton } from './styles'
-import { SourceCodeDebugger } from './SourceCodeDebugger'
+import { SourceCodePanel } from './SourceCodePanel'
 
 export const BytecodePanel = (): JSX.Element => {
   const ref = React.useRef<HTMLDivElement>(null)
@@ -20,9 +21,10 @@ export const BytecodePanel = (): JSX.Element => {
   const toggleSourceView = () => setSourceView((prev) => !prev)
 
   const activeBlock = useTypedSelector((state) => state.activeBlock)
-  const sourceCode = useTypedSelector(
-    (state) => state.sourceCodes.entities[activeBlock.address],
+  const sourceCode = useTypedSelector((state) =>
+    sourceCodesSelectors.selectById(state.sourceCodes, activeBlock.address),
   )?.sourceCode
+
   const activeStrucLog = useTypedSelector(
     (state) => state.structLogs.activeStructLog,
   )
@@ -42,6 +44,9 @@ export const BytecodePanel = (): JSX.Element => {
     }
   }, [activeStrucLog, activeBlockBytecode.disassembled])
 
+  if (isSourceView)
+    return <SourceCodePanel close={toggleSourceView} sourceCode={sourceCode} />
+
   if (!activeBlockBytecode?.disassembled)
     return (
       <StyledSmallPanel>
@@ -54,44 +59,37 @@ export const BytecodePanel = (): JSX.Element => {
   return (
     <StyledSmallPanel>
       <StyledHeading>
-        {isSourceView ? 'Source Code' : 'Disassembled Bytecode'}
-        {sourceCode || isSourceView ? (
-          <StyledButton
-            variant="text"
-            onClick={toggleSourceView}
-            key={isSourceView ? 'byte' : 'source'}
-          >
-            View {isSourceView ? 'bytecode' : 'source'}
+        Disassembled Bytecode
+        {sourceCode ? (
+          <StyledButton variant="text" onClick={toggleSourceView}>
+            View source
           </StyledButton>
         ) : null}
       </StyledHeading>
-      {isSourceView ? (
-        <SourceCodeDebugger source={sourceCode} />
-      ) : (
-        <StyledListWrapper ref={ref}>
-          <ViewportList
-            ref={listRef}
-            viewportRef={ref}
-            items={activeBlockBytecode.disassembled}
-            withCache={true}
-          >
-            {(item) => {
-              const { opcode, operand, pc } = item
-              const isActive =
-                activeStrucLog?.pc === ethers.BigNumber.from(pc).toNumber()
-              return (
-                <ExplorerListRow
-                  key={pc}
-                  chipValue={operand}
-                  isActive={isActive}
-                  opCode={convertOpcodeToName(opcode)}
-                  pc={pc}
-                />
-              )
-            }}
-          </ViewportList>
-        </StyledListWrapper>
-      )}
+
+      <StyledListWrapper ref={ref}>
+        <ViewportList
+          ref={listRef}
+          viewportRef={ref}
+          items={activeBlockBytecode.disassembled}
+          withCache={true}
+        >
+          {(item) => {
+            const { opcode, operand, pc } = item
+            const isActive =
+              activeStrucLog?.pc === ethers.BigNumber.from(pc).toNumber()
+            return (
+              <ExplorerListRow
+                key={pc}
+                chipValue={operand}
+                isActive={isActive}
+                opCode={convertOpcodeToName(opcode)}
+                pc={pc}
+              />
+            )
+          }}
+        </ViewportList>
+      </StyledListWrapper>
     </StyledSmallPanel>
   )
 }
