18e0cb958cb27292e553994badf88c12d2e38d2f
wolszczak96 <wolszczak96@gmail.com>
2023-04-13 17:23:42 +0200

    add comment

--- packages/transaction-trace-provider/src/hardhat.config.ts
+++ packages/transaction-trace-provider/src/hardhat.config.ts
@@ -11,6 +11,12 @@ const config: HardhatUserConfig = {
         url: process.env.HARDHAT_FORKING_URL,
       },
       chains: {
+        // chains are configured to fork from blocks around the time of
+        // March 15th 2023. it seems like a safe timespan for our use case.
+
+        // CAUTION:
+        // we shouldn't fork from too old blocks, because of getting errors of
+        // too many requests per second
         137: {
           hardforkHistory: {
             london: 40400000,
