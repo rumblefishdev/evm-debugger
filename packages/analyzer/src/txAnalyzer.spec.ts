6ec0346c95a596c8dde6383d7ceffd9fd1ed67d2
Rafał Lizoń <rafal.lizon@rumblefish.pl>
2023-04-25 18:07:28 +0200

    Fix

--- packages/analyzer/src/txAnalyzer.spec.ts
+++ packages/analyzer/src/txAnalyzer.spec.ts
@@ -6,10 +6,6 @@ import { prepareAnalyzer } from '../scripts/scriptHelper'
 
 import { TxAnalyzer } from './txAnalyzer'
 
-const removeWhiteSpaces = (data: string) => {
-  return data.replace(/\s/g, '')
-}
-
 async function runAnalyzerForTestDataFile(path: string) {
   const testData = await promises.readFile(path, 'utf8')
   const jsonTestData = JSON.parse(testData)
@@ -29,41 +25,45 @@ async function runAnalyzerForTestDataFile(path: string) {
 
 describe('TxAnalyzer', () => {
   describe('analyze transaction', () => {
+    beforeAll(() => {
+      jest.setTimeout(20_000)
+    })
+
     it('analyzes simple mint transaction', async () => {
       const result = await runAnalyzerForTestDataFile('./test/simpleMintTransactionLogs.json')
       expect(result).toMatchSnapshot()
-    }, 20_000)
+    })
 
     it('analyze transaction with revert', async () => {
       const result = await runAnalyzerForTestDataFile('./test/revertedTransactionLogs.json')
       expect(result).toMatchSnapshot()
-    }, 20_000)
+    })
 
     it('analyzes failed transaction', async () => {
       const result = await runAnalyzerForTestDataFile('./test/failedTransactionLogs.json')
       expect(result).toMatchSnapshot()
-    }, 20_000)
+    })
 
     it('analyzes failed transaction with extended errors', async () => {
       const result = await runAnalyzerForTestDataFile('./test/failedTransactionLogs2.json')
       expect(result).toMatchSnapshot()
-    }, 20_000)
+    })
 
     it('Transaction with empty struct log', async () => {
       await expect(runAnalyzerForTestDataFile('./test/transactionWithEmptyStructLogs.json')).rejects.toThrow(
         'Too primitive transaction without stack calls.',
       )
-    }, 20_000)
+    })
 
     it('analyzes transaction with invalid function with the same sighash as cached abis', async () => {
       // Analyzer is trying 1st decode using cached ABIs. In this tx 'transferFrom' have not standard(erc20) output.
       const result = await runAnalyzerForTestDataFile('./test/txWithInvalidOutputToDecode.json')
       expect(result).toMatchSnapshot()
-    }, 20_000)
+    })
 
     it('analyze transaction with contract deployment', async () => {
       const result = await runAnalyzerForTestDataFile('./test/createContract.json')
       expect(result).toMatchSnapshot()
-    }, 20_000)
+    })
   })
 })
