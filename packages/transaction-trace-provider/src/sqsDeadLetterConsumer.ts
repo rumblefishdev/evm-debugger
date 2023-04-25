2a74754b416c6d09f87417d60fac0c90d942e44a
Rafał Lizoń <rafal.lizon@rumblefish.pl>
2023-04-25 11:48:40 +0200

    Fix for node patch and DLQ sqs

--- packages/transaction-trace-provider/src/sqsDeadLetterConsumer.ts
+++ packages/transaction-trace-provider/src/sqsDeadLetterConsumer.ts
@@ -4,9 +4,8 @@ import { TransactionTraceResponseStatus } from '@evm-debuger/types'
 import { putTxEventToDdb } from './ddb'
 
 export const consumeSqsAnalyzeTxError: Handler = async (event: SQSEvent) => {
-  const records = event.Records
-  if (records && records.length > 0) {
-    const txHash = records[0].messageAttributes.txHash.stringValue!
+  for (const record of event.Records) {
+    const txHash = record.messageAttributes.txHash.stringValue!
     await putTxEventToDdb(TransactionTraceResponseStatus.FAILED, txHash)
   }
 }
