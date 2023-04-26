befdf34ca4381d9473173dd2e68dcf4f65aec897
PiotrRumblefish <piotr.sierankiewicz@rumblefish.pl>
2023-04-26 13:12:21 +0200

    update

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -7,18 +7,21 @@ import { PersistGate } from 'redux-persist/integration/react'
 
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import { persistor, store } from './store/store'
+import { isPersistOn, persistor, store } from './store/store'
 
 const root = ReactDOM.createRoot(document.querySelector('#root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate
-        loading={true}
-        persistor={persistor}
-      >
-        <App />
-      </PersistGate>
+      {isPersistOn && (
+        <PersistGate
+          loading={true}
+          persistor={persistor}
+        >
+          <App />
+        </PersistGate>
+      )}
+      {!isPersistOn && <App />}
     </Provider>
   </React.StrictMode>,
 )
