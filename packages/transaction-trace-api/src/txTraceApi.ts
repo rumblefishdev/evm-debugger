1c109314597d9662ec359415d1d611d8dc21e44e
Rafał Lizoń <rafal.lizon@rumblefish.pl>
2023-05-16 15:08:31 +0200

    Test

--- packages/transaction-trace-api/src/txTraceApi.ts
+++ packages/transaction-trace-api/src/txTraceApi.ts
@@ -35,6 +35,7 @@ export const createResponse = (status: string, output = {}) => {
 export const analyzeTransactionHandler = async (
   event: APIGatewayProxyEvent,
 ): Promise<APIGatewayProxyResult> => {
+  console.log('RAFAL test')
   const { txHash, chainId } = event.pathParameters!
   if (!txHash || !chainId)
     return createResponse(
