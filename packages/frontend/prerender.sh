7fe94fc1bbc56ec27daaf73120f183e38b7e33b6
Filip Mazur <fmazur677@gmail.com>
2023-06-20 12:02:44 +0200

    ater codeReview - DONE

--- packages/frontend/prerender.sh
+++ packages/frontend/prerender.sh
@@ -8,3 +8,6 @@ mv build2 build/evm-debugger
 cp build/evm-debugger/index.html build/index.html 
 react-spa-prerender 
 mv build/evm-debugger.html build/evm-debugger/index.html
+mv build build2
+mv build2/evm-debugger build/
+rm -rf build2
