b8d5b7bb11fbbf51097f7627dc92ab00c9ff6222
Marcin Misiorek <marcin.misiorek@rumblefish.pl>
2023-06-23 11:04:56 +0200

    Add new building script - part 2

--- packages/frontend/build.sh
+++ packages/frontend/build.sh
@@ -4,14 +4,40 @@ set -e
 
 ENV=${2:-"local"}
 
+function print_msg() {
+  echo "#############################"
+  echo "##"
+  echo "##  $1 "
+  echo "##"
+  echo "#############################"
+}
+
+function fix_scripts() {
+  local filename="$1"
+  local new_script_name=$(ls build/static/js | grep -E '^main\..*.js$')
+
+  sed "s|/evm-debugger/static/js/main\.[0-9A-Za-z]*\.js|/evm-debugger/static/js/$new_script_name|g" "$filename" > "$filename.new"
+  mv "$filename.new" "$filename"
+}
+
 function build() {
   local prerender=${1:-"false"}
 
-  if [[ "$ENV" != "" ]]; then
-    REACT_APP_IS_PRERENDER=$prerender PUBLIC_URL=/evm-debugger dotenv -e ".env.$ENV" react-scripts build
-  else
-    REACT_APP_IS_PRERENDER=$prerender PUBLIC_URL=/evm-debugger EXTEND_ESLINT=true react-scripts build
+  if [[ "$prerender" != "true" ]]; then
+    print_msg "Building"
   fi
+
+  export PUBLIC_URL=/evm-debugger
+  export REACT_APP_IS_PRERENDER=$prerender
+
+  if [[ "$ENV" != "" ]]; then
+     dotenv -e ".env.$ENV" react-scripts build
+  else
+     react-scripts build
+  fi
+
+  unset PUBLIC_URL
+  unset REACT_APP_IS_PRERENDER
 }
 
 function swap_to_evm_debugger_dir() {
@@ -21,25 +47,39 @@ function swap_to_evm_debugger_dir() {
 }
 
 function prerender() {
+  print_msg "Building prerender page"
   mv build original_build
 
   build "true"
 
   swap_to_evm_debugger_dir build
-  cp build/evm-debugger/index.html build/index.html
+  mv original_build/index.html original_build/non-static.html
   react-spa-prerender
   mv build prerendered_build
-  ls -lh prerendered_build
 
   swap_to_evm_debugger_dir original_build
-  mv original_build/evm-debugger/index.html original_build/evm-debugger/non-static.html
   mv prerendered_build/evm-debugger.html original_build/evm-debugger/index.html
-  rm -rf prerendered_build
-  mv original_build build/
+  mv original_build/evm-debugger build/
+  rm -rf prerendered_build original_build
+
+  fix_scripts build/index.html
+  fix_scripts build/non-static.html
+}
+
+function serve_prerender() {
+  build
+  prerender
+
+  print_msg "Serving application"
+
+  swap_to_evm_debugger_dir build
+  cd build && http-server -o /evm-debugger
 }
 
 if [[ "$1" == "build" ]]; then
   build
 elif [[ "$1" == "prerender" ]]; then
   prerender
+elif [[ "$1" == "serve-prerender" ]]; then
+  serve_prerender
 fi
