8f105a70a6148e25466f17c1fd73f24dafbb0e38
Marcin Misiorek <marcin.misiorek@rumblefish.pl>
2023-06-26 12:59:09 +0200

    Comment out GA code temporarly

--- packages/frontend/src/components/GAnalytics/GAnalytics.tsx
+++ packages/frontend/src/components/GAnalytics/GAnalytics.tsx
@@ -9,10 +9,10 @@ const PROD_URL = 'www.rumblefish.dev'
 
 const onProd = () => typeof window !== 'undefined' && window.location.hostname === PROD_URL
 export const GAnalyticsInit = () => {
-  if (onProd()) {
-    ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_ID)
-    TagManager.initialize({ gtmId: process.env.REACT_APP_GOOGLE_TAG_MANAGER_ID })
-  }
+  // if (onProd()) {
+  //   ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_ID)
+  //   TagManager.initialize({ gtmId: process.env.REACT_APP_GOOGLE_TAG_MANAGER_ID })
+  // }
 }
 
 export const GAnalytics = () => {
