d285f5ac5ee37ec1f6a4f05e772a0c78b27c9376
Marcin Misiorek <marcin.misiorek@rumblefish.pl>
2023-06-26 13:19:14 +0200

    Remove unused variables

--- packages/frontend/src/components/GAnalytics/GAnalytics.tsx
+++ packages/frontend/src/components/GAnalytics/GAnalytics.tsx
@@ -1,13 +1,13 @@
 import { useLocation } from 'react-router-dom'
 import React from 'react'
-import ReactGA from 'react-ga4'
+// import ReactGA from 'react-ga4'
 // import TagManager from 'react-gtm-module'
 
-import { ROUTES } from '../../routes'
+// import { ROUTES } from '../../routes'
 
 const PROD_URL = 'www.rumblefish.dev'
 
-const onProd = () => typeof window !== 'undefined' && window.location.hostname === PROD_URL
+// const onProd = () => typeof window !== 'undefined' && window.location.hostname === PROD_URL
 export const GAnalyticsInit = () => {
   // if (onProd()) {
   //   ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_ID)
@@ -16,7 +16,7 @@ export const GAnalyticsInit = () => {
 }
 
 export const GAnalytics = () => {
-  const location = useLocation()
+  // const location = useLocation()
   // if (onProd())
   //   React.useEffect(() => {
   //     const path = `${ROUTES.BASE}${location.pathname}`
