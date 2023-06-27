5a7a8204d257cacf341a50891a1dd3c754adf179
Filip Mazur <fmazur677@gmail.com>
2023-06-27 09:47:17 +0200

    remove logs

--- packages/frontend/src/components/GAnalytics/GAnalytics.tsx
+++ packages/frontend/src/components/GAnalytics/GAnalytics.tsx
@@ -1,7 +1,6 @@
 import { useLocation } from 'react-router-dom'
 import React from 'react'
 import ReactGA from 'react-ga4'
-// import TagManager from 'react-gtm-module'
 
 import { ROUTES } from '../../routes'
 
@@ -9,17 +8,11 @@ const PROD_URL = 'www.rumblefish.dev'
 
 const onProd = () => typeof window !== 'undefined' && window.location.hostname === PROD_URL
 export const GAnalyticsInit = () => {
-  console.log('onProd value in GAnalyticsInit:', onProd())
-  if (onProd()) {
-    console.log('GAnalyticsInit function is running')
-    ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_ID)
-    // TagManager.initialize({ gtmId: process.env.REACT_APP_GOOGLE_TAG_MANAGER_ID })
-  }
+  if (onProd()) ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_ID)
 }
 
 export const GAnalytics = () => {
   const location = useLocation()
-  console.log('onProd value in GAnalytics:', onProd())
   if (onProd())
     React.useEffect(() => {
       const path = `${ROUTES.BASE}${location.pathname}`
