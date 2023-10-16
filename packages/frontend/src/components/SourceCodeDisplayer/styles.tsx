38c4ce691d7c64d84d4efdd728c9e35d6598fafc
Misieq01 <38589417+Misieq01@users.noreply.github.com>
2023-10-16 09:52:13 +0200

    temporary fix for line highlighting

--- packages/frontend/src/components/SourceCodeDisplayer/styles.tsx
+++ packages/frontend/src/components/SourceCodeDisplayer/styles.tsx
@@ -55,11 +55,12 @@ const SyntaxHighlighter = ({ source, highlightStartLine, highlightEndLine }: Syn
   const textHeight = lines ? lines.length : 0
   const height = textHeight * ACE_LINE_HEIGHT + BORDER
 
+  // Temporary solution for mismatching line numbers between Ace and Analyzer output
   const highlightMarker: IMarker = {
     type: 'fullLine',
-    startRow: highlightStartLine,
-    startCol: 0,
-    endRow: highlightEndLine,
+    startRow: highlightStartLine - 1,
+    startCol: 1,
+    endRow: highlightEndLine - 1,
     endCol: 0,
     // eslint-disable-next-line unicorn/no-keyword-prefix -- Ace require to provide className in that form
     className: 'highlightMarker',
