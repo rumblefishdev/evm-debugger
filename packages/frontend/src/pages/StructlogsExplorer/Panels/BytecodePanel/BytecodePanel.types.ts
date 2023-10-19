bf26da4f64951dc681d665acdd57f765468db5a8
Misieq01 <38589417+Misieq01@users.noreply.github.com>
2023-10-19 11:30:51 +0200

    Update BytecodePanel.types.ts

--- packages/frontend/src/pages/StructlogsExplorer/Panels/BytecodePanel/BytecodePanel.types.ts
+++ packages/frontend/src/pages/StructlogsExplorer/Panels/BytecodePanel/BytecodePanel.types.ts
@@ -1,11 +1,4 @@
-import type { ForwardedRef } from 'react'
-
 import type { TDisassembledBytecode } from '../../../../store/bytecodes/bytecodes.types'
-
-export interface BytecodePanelProps {
-  toggleSourceCodePanel: () => void
-  isSourceCodeAvailable: boolean
-}
 
 export interface BytecodePanelContainerProps {
   toggleSourceCodePanel: () => void
