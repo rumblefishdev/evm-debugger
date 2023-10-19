0fc1dd5d4fb6160d466f5ca2067f066d860f757e
Mariusz Korzekwa <maledorak@gmail.com>
2023-10-19 15:04:50 +0200

    Improve traceLogs selector

--- packages/frontend/src/store/traceLogs/traceLogs.selectors.ts
+++ packages/frontend/src/store/traceLogs/traceLogs.selectors.ts
@@ -3,8 +3,12 @@ import { createSelector } from '@reduxjs/toolkit'
 import { StoreKeys } from '../store.keys'
 import { selectReducer } from '../store.utils'
 
+import { traceLogsAdapter } from './traceLogs.slice'
+
 const selectTraceLogsState = createSelector([selectReducer(StoreKeys.TRACE_LOGS)], (state) => state)
 
-const selectAll = createSelector(selectTraceLogsState, (state) => state)
+const selectAll = createSelector([selectTraceLogsState], (state) => traceLogsAdapter.getSelectors().selectAll(state))
 
-export const traceLogsSelectors = { selectAll }
+const selectEntities = createSelector([selectTraceLogsState], (state) => traceLogsAdapter.getSelectors().selectEntities(state))
+
+export const traceLogsSelectors = { selectEntities, selectAll }
