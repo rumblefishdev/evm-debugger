8a4dbe9ee108dbd80462f59de11a10effbd17284
Misieq01 <38589417+Misieq01@users.noreply.github.com>
2023-10-24 11:49:56 +0200

    Delete StructlogPanel.styles.tsx

--- packages/frontend/src/pages/StructlogsExplorer/Panels/StructlogPanel/StructlogPanel.styles.tsx
+++
@@ -1,9 +0,0 @@
-import { styled } from '@mui/material'
-import { FixedSizeList } from 'react-window'
-
-export const StyledVirtualisedList = styled(FixedSizeList)(({ theme }) => ({
-  borderTopLeftRadius: '3px',
-  borderBottomLeftRadius: '3px',
-  borderBottom: `1px solid ${theme.palette.rfLinesLight}`,
-  ...theme.customStyles.scrollbar,
-}))
