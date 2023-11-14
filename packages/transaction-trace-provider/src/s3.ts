f6034fc3d2fbd20fa4b9ae90566b315c0fe44d41
Misieq01 <38589417+Misieq01@users.noreply.github.com>
2023-11-14 09:17:47 +0100

    save

--- packages/transaction-trace-provider/src/s3.ts
+++ packages/transaction-trace-provider/src/s3.ts
@@ -1,10 +1,24 @@
-import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
+import type { CompletedPart } from '@aws-sdk/client-s3'
+import {
+  CreateMultipartUploadCommand,
+  UploadPartCommand,
+  CompleteMultipartUploadCommand,
+  AbortMultipartUploadCommand,
+  PutObjectCommand,
+  S3Client,
+} from '@aws-sdk/client-s3'
 
 const s3Client = new S3Client({
   region: process.env.AWS_REGION,
 })
 
+export const getFilePath = (txHash: string, chainId: string) => {
+  const fileName = `trace/${chainId}/${txHash}.json`
+  return `${process.env.ANALYZER_DATA_BUCKET_NAME}/${fileName}`
+}
+
 export const pushTraceToS3 = async (txHash: string, chainId: string, json: string) => {
+  console.log('process.env.ANALYZER_DATA_BUCKET_NAME', process.env.ANALYZER_DATA_BUCKET_NAME)
   const fileName = `trace/${chainId}/${txHash}.json`
   const params = {
     Key: fileName,
@@ -17,4 +31,55 @@ export const pushTraceToS3 = async (txHash: string, chainId: string, json: strin
   const command = new PutObjectCommand(params)
   await s3Client.send(command)
   return filePath
+}
+
+export const createMultiPartUpload = async (txHash: string, chainId: string) => {
+  const fileName = `trace/${chainId}/${txHash}.json`
+  const params = {
+    Key: fileName,
+    ContentType: 'application/json',
+    Bucket: process.env.ANALYZER_DATA_BUCKET_NAME,
+  }
+  const command = new CreateMultipartUploadCommand(params)
+  const response = await s3Client.send(command)
+  return response.UploadId
+}
+
+export const uploadPart = async (txHash: string, chainId: string, uploadId: string, partNumber: number, body: string) => {
+  const fileName = `trace/${chainId}/${txHash}.json`
+  const params = {
+    UploadId: uploadId,
+    PartNumber: partNumber,
+    Key: fileName,
+    Bucket: process.env.ANALYZER_DATA_BUCKET_NAME,
+    Body: body,
+  }
+  const command = new UploadPartCommand(params)
+  const response = await s3Client.send(command)
+  return response.ETag
+}
+
+export const completeMultiPartUpload = async (txHash: string, chainId: string, uploadId: string, parts: CompletedPart[]) => {
+  const fileName = `trace/${chainId}/${txHash}.json`
+  const params = {
+    UploadId: uploadId,
+    MultipartUpload: {
+      Parts: parts,
+    },
+    Key: fileName,
+    Bucket: process.env.ANALYZER_DATA_BUCKET_NAME,
+  }
+  const command = new CompleteMultipartUploadCommand(params)
+  await s3Client.send(command)
+}
+
+export const abortMultiPartUpload = async (txHash: string, chainId: string, uploadId: string) => {
+  const fileName = `trace/${chainId}/${txHash}.json`
+  const params = {
+    UploadId: uploadId,
+    Key: fileName,
+    Bucket: process.env.ANALYZER_DATA_BUCKET_NAME,
+  }
+  const command = new AbortMultipartUploadCommand(params)
+  await s3Client.send(command)
 }
--- packages/transaction-trace-provider/src/sqsConsumer.ts
+++ packages/transaction-trace-provider/src/sqsConsumer.ts
@@ -5,11 +5,12 @@ import { reset } from '@nomicfoundation/hardhat-network-helpers'
 import type { TRawTransactionTraceResult } from '@evm-debuger/types'
 import { TransactionTraceResponseStatus } from '@evm-debuger/types'
 import { AWSLambda, captureException } from '@sentry/serverless'
+import type { CompletedPart } from '@aws-sdk/client-s3'
 
 import { version } from '../package.json'
 
 import { putTxEventToDdb } from './ddb'
-import { pushTraceToS3 } from './s3'
+import { pushTraceToS3, getFilePath, abortMultiPartUpload, completeMultiPartUpload, createMultiPartUpload, uploadPart } from './s3'
 import { DEFAULT_ERROR, knownErrors } from './errors'
 
 AWSLambda.init({
@@ -25,26 +26,84 @@ export const processTx = async (txHash: string, chainId: string, hardhatForkingU
   const hardhatProvider = await hardhat.run(TASK_NODE_GET_PROVIDER, {
     chainId,
   })
+  console.log(`Provider for ${chainId} is ${hardhatProvider}`)
   console.log(`Starting debug_traceTransaction for ${txHash}`)
   const traceResult: TRawTransactionTraceResult = await hardhatProvider.send('debug_traceTransaction', [txHash])
   // TODO: fix in https://github.com/rumblefishdev/evm-debugger/issues/285
   // traceResult.structLogs = traceResult.structLogs.map((structLog, index) => ({ ...structLog, index }))
 
-  return pushTraceToS3(txHash, chainId, JSON.stringify(traceResult))
+  console.log(`Finished debug_traceTransaction for ${txHash}`)
+  console.log(`traceResult.structLogs.length`, traceResult.structLogs.length)
+
+  const uploadId = await createMultiPartUpload(txHash, chainId)
+
+  if (!uploadId) throw new Error('Failed to create multi part upload')
+  let j = 1
+
+  const rootPartBody = JSON.stringify({
+    structLogs: traceResult.structLogs.slice(0, 1000),
+    returnValue: traceResult.returnValue,
+    gas: traceResult.gas,
+    failed: traceResult.failed,
+  })
+
+  const rootPart = {
+    PartNumber: j,
+    ETag: `entityTag-${j}`,
+    body: JSON.stringify({
+      structLogs: traceResult.structLogs.slice(0, 1000),
+      returnValue: traceResult.returnValue,
+      gas: traceResult.gas,
+      failed: traceResult.failed,
+    }),
+  }
+
+  try {
+    const parts: { PartNumber: number; body: string }[] = [rootPart]
+    const uploadedParts: CompletedPart[] = []
+
+    for (let index = 1000; index < traceResult.structLogs.length; ) {
+      j++
+      const partNumber = j
+      const body = JSON.stringify(traceResult.structLogs.slice(index, index + 1000))
+      parts.push({ PartNumber: partNumber, body })
+      index += 1000
+    }
+
+    for await (const part of parts) {
+      console.log(`Uploading part ${part.PartNumber}`)
+      const partETag = await uploadPart(txHash, chainId, uploadId, part.PartNumber, part.body)
+      if (!partETag) throw new Error(`Failed to upload part: ${part.PartNumber}`)
+      uploadedParts.push({ PartNumber: part.PartNumber, ETag: partETag })
+    }
+
+    await completeMultiPartUpload(txHash, chainId, uploadId, uploadedParts)
+
+    console.log(`Finished uploading parts for ${txHash}`)
+  } catch (error) {
+    console.log(`Failed to upload to s3 for ${txHash}`)
+    await abortMultiPartUpload(txHash, chainId, uploadId)
+    throw error
+  }
 }
 
 export const consumeSqsAnalyzeTx: Handler = async (event: SQSEvent) => {
+  console.log('consumeSqsAnalyzeTx event', JSON.stringify(event))
   const records = event.Records
+  console.log(`Received ${records.length} records`)
+  console.log(JSON.stringify(records))
   if (records && records.length > 0) {
     const txHash = records[0].messageAttributes.txHash.stringValue!
     const chainId = records[0].messageAttributes.chainId.stringValue!
     const hardhatForkingUrl = records[0].messageAttributes.hardhatForkingUrl.stringValue!
+    console.log(JSON.stringify({ txHash, hardhatForkingUrl, chainId }))
     await putTxEventToDdb(TransactionTraceResponseStatus.RUNNING, txHash)
     try {
-      const s3TracePath = await processTx(txHash, chainId, hardhatForkingUrl)
-      await putTxEventToDdb(TransactionTraceResponseStatus.SUCCESS, txHash, {
-        s3Location: s3TracePath,
-      })
+      await processTx(txHash, chainId, hardhatForkingUrl)
+      const filePath = getFilePath(txHash, chainId)
+      putTxEventToDdb(TransactionTraceResponseStatus.SUCCESS, txHash, { filePath })
+      console.log(`Finished processing ${txHash}`)
+      console.log(`Trace saved to ${filePath}`)
     } catch (error) {
       const errorMessage = { errorDetails: DEFAULT_ERROR }
       if (error instanceof Error) {
