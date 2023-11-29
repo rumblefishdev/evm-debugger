9921a34e4fc6a6ee27f613c169c0f985b6a18c18
Mariusz Korzekwa <maledorak@gmail.com>
2023-11-29 09:03:15 +0100

    Fix srcmap api fetcher queuing process (#343)
    * Fix srcmap api fetcher queuing process
    
    * Fix addresses duplicates
    
    * Add lambdaRequestId to improve logs
    
    * lint:fix
    
    * Frontend and addresses fixes
    
    * Fix tests
    
    * Linter fix
    
    * Fix tests
    
    ---------
    
    Co-authored-by: Misieq01 <38589417+Misieq01@users.noreply.github.com>
    Co-authored-by: Marcin Misiorek <marcin.misiorek@rumblefish.pl>

--- packages/srcmap-api/src/lambdaApi.ts
+++ packages/srcmap-api/src/lambdaApi.ts
@@ -1,6 +1,6 @@
 /* eslint-disable import/exports-last */
 /* eslint-disable no-await-in-loop */
-import type { APIGatewayProxyEvent } from 'aws-lambda'
+import type { APIGatewayProxyEvent, Context } from 'aws-lambda'
 import { SrcMapStatus } from '@evm-debuger/types'
 import type { ISrcMapApiPayload, TSrcMapAddres } from '@evm-debuger/types'
 import { AWSLambda, captureException } from '@sentry/serverless'
@@ -21,6 +21,7 @@ AWSLambda.setTag('lambda_name', 'srcmap-api')
 
 export const addressesProcessing = async (
   contractAddressObj: TSrcMapAddres,
+  awsRequestId: string,
 ): Promise<ISrcMapApiPayload> => {
   console.log(contractAddressObj.address, '/Processing/Start')
 
@@ -46,10 +47,15 @@ export const addressesProcessing = async (
       SrcMapStatus.SOURCE_DATA_FETCHING_QUEUED_FAILED,
       SrcMapStatus.SOURCE_DATA_FETCHING_FAILED,
       SrcMapStatus.FILES_EXTRACTING_FAILED,
-    ].includes(payload.status)
+      SrcMapStatus.SOURCE_DATA_FETCHING_QUEUED_TO_DLQ,
+    ].includes(payload.status) ||
+    // Run fetch queuing if last fetch was more than 30 seconds ago
+    (payload.status === SrcMapStatus.SOURCE_DATA_FETCHING_QUEUED_SUCCESS &&
+      payload.timestamp !== undefined &&
+      Date.now() - 30000 > payload.timestamp)
   ) {
     console.log(payload.address, '/Queuing Contract Fetch/Initialing')
-    payload = await triggerFetchSourceCode(payload)
+    payload = await triggerFetchSourceCode(payload, awsRequestId)
   }
 
   console.log(payload.address, '/Payload', JSON.stringify(payload, null, 2))
@@ -60,55 +66,100 @@ export const addressesProcessing = async (
     [SrcMapStatus.FILES_EXTRACTING_SUCCESS].includes(payload.status)
   ) {
     console.log(payload.address, '/Compiler Trigger/Initialing')
-    payload = await triggerSourceMapCompiler(payload)
+    payload = await triggerSourceMapCompiler(payload, awsRequestId)
   }
 
   console.log(contractAddressObj.address, '/Processing/Done')
   return payload
 }
 
-export const srcmapApiHandler = async (event: APIGatewayProxyEvent) => {
+export const srcmapApiHandler = async (
+  event: APIGatewayProxyEvent,
+  context: Context,
+) => {
   let addresses: TSrcMapAddres[] = []
+  let transactionHash: string | undefined
 
   if (event.body) {
     addresses = JSON.parse(event.body)?.addresses
+    transactionHash = JSON.parse(event.body)?.transactionHash
   }
 
   if (!addresses || addresses.length === 0)
-    return createResponse({
-      status: SrcMapStatus.FAILED,
-      error: 'Invalid params - No addresses provided',
+    return createResponse(
+      {
+        status: SrcMapStatus.FAILED,
+        error: 'Invalid params - No addresses provided',
+      },
+      context.awsRequestId,
+    )
+
+  if (!transactionHash) {
+    return createResponse(
+      {
+        status: SrcMapStatus.FAILED,
+        error: 'Invalid params - No transaction hash provided',
+      },
+      context.awsRequestId,
+    )
+  }
+
+  // Remove duplicates
+  addresses = addresses
+    .map((addressObj) => ({
+      ...addressObj,
+      address: addressObj.address.toLowerCase(),
+    }))
+    .filter((addressObj, index, self) => {
+      const foundIndex = self.findIndex(
+        (element) => element.address === addressObj.address,
+      )
+      return foundIndex === index
     })
 
-  console.log('Processing\n', JSON.stringify(addresses, null, 2))
+  console.log(
+    `Processing transaction: ${transactionHash}\nWith contracts:\n`,
+    JSON.stringify(addresses, null, 2),
+  )
   try {
     const responseContainer = (
       await Promise.all(
-        addresses.map((addressObj) => addressesProcessing(addressObj)),
+        addresses.map((addressObj) =>
+          addressesProcessing(addressObj, context.awsRequestId),
+        ),
       )
     ).reduce((accumulator: Record<string, ISrcMapApiPayload>, element) => {
       accumulator[element.address] = element
       return accumulator
     }, {})
 
-    return createResponse({
-      status: SrcMapStatus.SUCCESS,
-      data: responseContainer,
-    })
+    return createResponse(
+      {
+        status: SrcMapStatus.SUCCESS,
+        data: responseContainer,
+      },
+      context.awsRequestId,
+    )
   } catch (error) {
     if (error instanceof Error) {
       captureException(error)
-      return createResponse({
-        status: SrcMapStatus.FAILED,
-        error: error.message,
-      })
+      return createResponse(
+        {
+          status: SrcMapStatus.FAILED,
+          error: error.message,
+        },
+        context.awsRequestId,
+      )
     }
   }
 
-  return createResponse({
-    status: SrcMapStatus.FAILED,
-    error: 'Unknown error',
-  })
+  return createResponse(
+    {
+      status: SrcMapStatus.FAILED,
+      error: 'Unknown error',
+    },
+    context.awsRequestId,
+  )
 }
 
 export const srcmapApiEntrypoint = AWSLambda.wrapHandler(srcmapApiHandler)
--- packages/srcmap-api/src/wrappers.ts
+++ packages/srcmap-api/src/wrappers.ts
@@ -1,7 +1,10 @@
 import type { ISrcMapApiResponseBody } from '@evm-debuger/types'
 import { SrcMapStatus } from '@evm-debuger/types'
 
-export const createResponse = (args: ISrcMapApiResponseBody) => {
+export const createResponse = (
+  args: ISrcMapApiResponseBody,
+  requestId: string,
+) => {
   const isError = [SrcMapStatus.FAILED].includes(args.status)
 
   const body: ISrcMapApiResponseBody = {
@@ -12,6 +15,7 @@ export const createResponse = (args: ISrcMapApiResponseBody) => {
   return {
     statusCode: isError ? 400 : 200,
     headers: {
+      'x-amzn-LambdaRequestId': requestId,
       'Access-Control-Allow-Origin': '*',
     },
     body: JSON.stringify(body),
