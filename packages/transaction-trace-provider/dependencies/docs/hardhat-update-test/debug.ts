e40b0c984c40a1cedf82c2d5ad8a9835ce58f8c2
kamils <kserafin2806@gmail.com>
2023-12-19 21:46:14 +0100

    add patch to make possibility to chunks structLogs on debugger side

--- packages/transaction-trace-provider/dependencies/docs/hardhat-update-test/debug.ts
+++ packages/transaction-trace-provider/dependencies/docs/hardhat-update-test/debug.ts
@@ -2,20 +2,22 @@ import hardhat from 'hardhat'
 import { reset } from '@nomicfoundation/hardhat-network-helpers'
 import { TASK_NODE_GET_PROVIDER } from 'hardhat/builtin-tasks/task-names'
 import 'dotenv/config'
+import {forkingUrlMap} from "./hardhat.config";
+const { structLogsEmitter } = require('hardhat/internal/hardhat-network/stack-traces/vm-debug-tracer.js');
 
 if (!process.env.ALCHEMY_KEY) throw Error("No ALCHEMY_KEY in .env")
 
 // Script for debugging hardhat's debug_traceTransaction
 // Run it via `npm run debug`
 
-//// Very big transaction, always failing without patch on USA based machines
-const txHash = '0x9aecfb4d762a13188d876c64baec7646c9b58f10605d4ef63bfb80d0eb5ec5a7'
+// Very big transaction, always failing without patch on USA based machines
+const txHash = '0xcfc0e43ce1ddb6ef2938819b5e90ec07aadf5bf2bfdb217ab81685f5acf5fb88'
 
 //// PUSH0 transaction
 // const txHash = '0x08d97335ac1913d382170eeffe5ace356daeab310bd7b83447bef34d5928a115'
 
-const chainId = '1'
-const hardhatForkingUrl = 'https://eth-mainnet.alchemyapi.io/v2/'
+const chainId = '137'
+const hardhatForkingUrl = forkingUrlMap[Number(chainId) as keyof typeof forkingUrlMap]
 
 reset(`${hardhatForkingUrl}${process.env.ALCHEMY_KEY}`)
   .then(() => {
@@ -23,6 +25,10 @@ reset(`${hardhatForkingUrl}${process.env.ALCHEMY_KEY}`)
       .then((hardhatProvider) => {
         console.log('Hardhat provider', hardhatProvider)
         console.log(`Starting debug_traceTransaction for ${txHash}`)
+
+        structLogsEmitter.on("structLog", (structLog: any) => {
+          console.log(structLog)
+        })
 
         hardhatProvider.send('debug_traceTransaction', [txHash])
           .then((traceResult: any) => {
--- packages/transaction-trace-provider/dependencies/docs/hardhat-update-test/hardhat.config.ts
+++ packages/transaction-trace-provider/dependencies/docs/hardhat-update-test/hardhat.config.ts
@@ -1,9 +1,28 @@
 import '@nomicfoundation/hardhat-toolbox'
 import { HardhatUserConfig } from 'hardhat/types'
-import { ChainId } from './chains'
 import 'dotenv/config'
 
 if (!process.env.ALCHEMY_KEY) throw Error("No ALCHEMY_KEY in .env")
+
+export enum ChainId {
+  mainnet = 1,
+  goerli = 5,
+  polygon = 137,
+  mumbai = 80001,
+  sepolia = 11155111,
+  arbitrum = 42161,
+  arbitrumGoerli = 421613,
+}
+
+export const forkingUrlMap = {
+  [ChainId.mainnet]: `https://eth-mainnet.alchemyapi.io/v2/`,
+  [ChainId.goerli]: `https://eth-goerli.alchemyapi.io/v2/`,
+  [ChainId.polygon]: `https://polygon-mainnet.g.alchemy.com/v2/`,
+  [ChainId.mumbai]: `https://polygon-mumbai.g.alchemy.com/v2/`,
+  [ChainId.sepolia]: `https://eth-sepolia.g.alchemy.com/v2/`,
+  [ChainId.arbitrum]: `https://arb-mainnet.g.alchemy.com/v2`,
+  [ChainId.arbitrumGoerli]: `https://arb-goerli.g.alchemy.com/v2`,
+}
 
 export const config: HardhatUserConfig = {
   paths: {
