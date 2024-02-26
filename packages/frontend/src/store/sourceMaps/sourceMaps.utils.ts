7eeed72a4a11450476a0e5d07ca9baa96f173864
Misieq01 <38589417+Misieq01@users.noreply.github.com>
2024-02-26 20:47:54 +0100

    save

--- packages/frontend/src/store/sourceMaps/sourceMaps.utils.ts
+++ packages/frontend/src/store/sourceMaps/sourceMaps.utils.ts
@@ -1,124 +1,138 @@
 import { NodeType } from '@evm-debuger/types'
-import type {
-  TYulBlock,
-  TYulNode,
-  TYulNodeBase,
-  TYulAssignment,
-  TYulExpressionStatement,
-  TYulForLoop,
-  TYulVariableDeclaration,
-  TYulTypedName,
-  TYulLiteral,
-  TYulFunctionCall,
-  TYulFunctionDefinition,
-  TYulIdentifier,
-  TYulIf,
-} from '@evm-debuger/types'
+import type { TYulBlock, TYulNode, TYulTypedName, TYulLiteral, TYulIdentifier } from '@evm-debuger/types'
 
-import type { TYulNodeElements } from '../yulNodes/yulNodes.types'
+import type {
+  TYulNodeElements,
+  TYulNodeLinkedElement,
+  TParsedYulAssignment,
+  TParsedYulBlock,
+  TParsedYulExpressionStatement,
+  TParsedYulForLoop,
+  TParsedYulFunctionCall,
+  TParsedYulFunctionDefinition,
+  TParsedYulIf,
+  TParsedYulVariableDeclaration,
+} from '../yulNodes/yulNodes.types'
 
 const createIdentifier = (src: string): string => src.split(':').slice(0, 2).join(':')
 
 export const convertYulTreeToArray = (yulTree: TYulBlock): TYulNodeElements => {
-  const yulNodesLinkArray: TYulNodeBase[] = []
-  const yulNodeBlocks: TYulBlock[] = []
-  const yulNodeAssignments: TYulAssignment[] = []
-  const yulExpressionStatements: TYulExpressionStatement[] = []
-  const yulFunctionDefinitions: TYulFunctionDefinition[] = []
-  const yulVariableDeclarations: TYulVariableDeclaration[] = []
-  const yulForLoops: TYulForLoop[] = []
-  const yulIfs: TYulIf[] = []
-  const yulFunctionCalls: TYulFunctionCall[] = []
+  const yulNodesLinkArray: TYulNodeLinkedElement[] = []
+  const yulNodeBlocks: TParsedYulBlock[] = []
+  const yulNodeAssignments: TParsedYulAssignment[] = []
+  const yulExpressionStatements: TParsedYulExpressionStatement[] = []
+  const yulFunctionDefinitions: TParsedYulFunctionDefinition[] = []
+  const yulVariableDeclarations: TParsedYulVariableDeclaration[] = []
+  const yulForLoops: TParsedYulForLoop[] = []
+  const yulIfs: TParsedYulIf[] = []
+  const yulFunctionCalls: TParsedYulFunctionCall[] = []
+  const yulTypedNames: TYulTypedName[] = []
+  const yulLiterals: TYulLiteral[] = []
+  const yulIdentifiers: TYulIdentifier[] = []
 
   const traverse = (node: TYulNode, parentNode?: TYulNode) => {
-    yulNodesLinkArray.push({ src: createIdentifier(node.src), nodeType: node.nodeType })
+    const identifier = createIdentifier(node.src)
 
     switch (node.nodeType) {
-      case NodeType.YulBlock:
-        yulNodeBlocks.push(node)
-        node.statements.forEach((child) => traverse(child, node))
+      case NodeType.YulBlock: {
+        yulNodesLinkArray.push({ rootSrc: identifier, rootNodeType: node.nodeType, elementSrc: identifier, elementNodeType: node.nodeType })
+        const mappedStatements = node.statements.map((child) => traverse(child, node))
+        yulNodeBlocks.push({ ...node, statements: mappedStatements })
         break
-      case NodeType.YulAssignment:
-        yulNodeAssignments.push(node)
+      }
+      case NodeType.YulAssignment: {
+        yulNodesLinkArray.push({ rootSrc: identifier, rootNodeType: node.nodeType, elementSrc: identifier, elementNodeType: node.nodeType })
+        const mappedValue = traverse(node.value)
+        const mappedVariableNames = node.variableNames.map((child) => traverse(child, node))
+        yulNodeAssignments.push({ ...node, variableNames: mappedVariableNames, value: mappedValue })
         break
-      case NodeType.YulExpressionStatement:
-        yulExpressionStatements.push(node)
+      }
+      case NodeType.YulExpressionStatement: {
+        yulNodesLinkArray.push({ rootSrc: identifier, rootNodeType: node.nodeType, elementSrc: identifier, elementNodeType: node.nodeType })
+        const mappedExpression = traverse(node.expression)
+        yulExpressionStatements.push({ ...node, expression: mappedExpression })
         break
-      case NodeType.YulFunctionDefinition:
-        yulFunctionDefinitions.push(node)
+      }
+      case NodeType.YulFunctionDefinition: {
+        yulNodesLinkArray.push({ rootSrc: identifier, rootNodeType: node.nodeType, elementSrc: identifier, elementNodeType: node.nodeType })
+        const mappedParameters = node.parameters?.map((child) => traverse(child, node))
+        const mappedReturnVariables = node.returnVariables?.map((child) => traverse(child, node))
+        const mappedBody = traverse(node.body)
+        yulFunctionDefinitions.push({ ...node, returnVariables: mappedReturnVariables, parameters: mappedParameters, body: mappedBody })
         break
-      case NodeType.YulVariableDeclaration:
-        yulVariableDeclarations.push(node)
+      }
+      case NodeType.YulVariableDeclaration: {
+        yulNodesLinkArray.push({ rootSrc: identifier, rootNodeType: node.nodeType, elementSrc: identifier, elementNodeType: node.nodeType })
+        const mappedValue = traverse(node.value)
+        const mappedVariables = node.variables.map((child) => traverse(child, node))
+        yulVariableDeclarations.push({ ...node, variables: mappedVariables, value: mappedValue })
         break
-      case NodeType.YulForLoop:
-        yulForLoops.push(node)
+      }
+      case NodeType.YulForLoop: {
+        yulNodesLinkArray.push({ rootSrc: identifier, rootNodeType: node.nodeType, elementSrc: identifier, elementNodeType: node.nodeType })
+        const mappedCondition = traverse(node.condition)
+        const mappedPost = traverse(node.post)
+        const mappedPre = traverse(node.pre)
+        const mappedBody = traverse(node.body)
+        yulForLoops.push({ ...node, pre: mappedPre, post: mappedPost, condition: mappedCondition, body: mappedBody })
         break
-      case NodeType.YulIf:
-        yulIfs.push(node)
+      }
+      case NodeType.YulIf: {
+        yulNodesLinkArray.push({ rootSrc: identifier, rootNodeType: node.nodeType, elementSrc: identifier, elementNodeType: node.nodeType })
+        const mappedCondition = traverse(node.condition)
+        const mappedBody = traverse(node.body)
+        yulIfs.push({ ...node, condition: mappedCondition, body: mappedBody })
         break
-      case NodeType.YulFunctionCall:
-        yulFunctionCalls.push(node)
+      }
+      case NodeType.YulFunctionCall: {
+        yulNodesLinkArray.push({ rootSrc: identifier, rootNodeType: node.nodeType, elementSrc: identifier, elementNodeType: node.nodeType })
+        const mappedFunctionName = traverse(node.functionName)
+        const mappedArguments = node.arguments.map((child) => traverse(child, node))
+        yulFunctionCalls.push({ ...node, functionName: mappedFunctionName, arguments: mappedArguments })
         break
+      }
+      case NodeType.YulTypedName: {
+        yulNodesLinkArray.push({ rootSrc: identifier, rootNodeType: node.nodeType, elementSrc: identifier, elementNodeType: node.nodeType })
+        yulTypedNames.push({ ...node })
+        break
+      }
+      case NodeType.YulLiteral: {
+        yulNodesLinkArray.push({ rootSrc: identifier, rootNodeType: node.nodeType, elementSrc: identifier, elementNodeType: node.nodeType })
+        yulLiterals.push({ ...node })
+        break
+      }
+      case NodeType.YulIdentifier: {
+        yulNodesLinkArray.push({ rootSrc: identifier, rootNodeType: node.nodeType, elementSrc: identifier, elementNodeType: node.nodeType })
+        yulIdentifiers.push({ ...node })
+        break
+      }
       default:
         break
     }
 
-    if (node.nodeType === NodeType.YulBlock) {
-      node.statements.forEach((child) => traverse(child, node))
-    }
-
-    if (node.nodeType === NodeType.YulIf) {
-      traverse(node.condition)
-      traverse(node.body)
-    }
-
-    if (node.nodeType === NodeType.YulForLoop) {
-      traverse(node.condition)
-      traverse(node.pre)
-      traverse(node.post)
-      traverse(node.body)
-    }
-
-    if (node.nodeType === NodeType.YulFunctionDefinition) {
-      node.parameters?.forEach((child) => traverse(child, node))
-      node.returnVariables?.forEach((child) => traverse(child, node))
-      traverse(node.body)
-    }
-
-    if (node.nodeType === NodeType.YulVariableDeclaration) {
-      traverse(node.value)
-    }
-
-    if (node.nodeType === NodeType.YulAssignment) {
-      traverse(node.value)
-    }
-
-    if (node.nodeType === NodeType.YulExpressionStatement) {
-      traverse(node.expression)
-    }
-
-    if (node.nodeType === NodeType.YulFunctionCall) {
-      node.arguments.forEach((child) => traverse(child, node))
-      traverse(node.functionName)
-    }
-
-    if (node.nodeType === NodeType.YulTypedName) {
-      // do nothing
-    }
-
-    if (node.nodeType === NodeType.YulLiteral) {
-      // do nothing
-    }
-
-    if (node.nodeType === NodeType.YulIdentifier) {
-      // do nothing
-    }
+    return identifier
   }
 
   traverse(yulTree)
 
+  console.log('yulNodesLinkArray', yulNodesLinkArray)
+
+  console.log('yulNodeBlocks', yulNodeBlocks)
+  console.log('yulNodeAssignments', yulNodeAssignments)
+  console.log('yulExpressionStatements', yulExpressionStatements)
+  console.log('yulFunctionDefinitions', yulFunctionDefinitions)
+  console.log('yulVariableDeclarations', yulVariableDeclarations)
+  console.log('yulForLoops', yulForLoops)
+  console.log('yulIfs', yulIfs)
+  console.log('yulFunctionCalls', yulFunctionCalls)
+  console.log('yulTypedNames', yulTypedNames)
+  console.log('yulLiterals', yulLiterals)
+  console.log('yulIdentifiers', yulIdentifiers)
+
+  console.log('===================================')
+
   return yulNodesLinkArray.reduce<TYulNodeElements>((accumulator, node, index) => {
-    accumulator[node.src] = { ...node, listIndex: index }
+    accumulator[node.rootSrc] = { ...node, listIndex: index }
     return accumulator
   }, {})
 }
--- packages/frontend/src/store/yulNodes/yulNodes.types.ts
+++ packages/frontend/src/store/yulNodes/yulNodes.types.ts
@@ -1,6 +1,36 @@
-import type { TYulNodeBase } from '@evm-debuger/types'
+import type {
+  NodeType,
+  TYulAssignment,
+  TYulBlock,
+  TYulExpressionStatement,
+  TYulForLoop,
+  TYulFunctionCall,
+  TYulFunctionDefinition,
+  TYulIf,
+  TYulVariableDeclaration,
+} from '@evm-debuger/types'
 
-export type TYulNodeBaseWithListIndex = TYulNodeBase & { listIndex: number }
+export type TParsedYulBlock = Omit<TYulBlock, 'statements'> & { statements: string[] }
+export type TParsedYulAssignment = Omit<TYulAssignment, 'variableNames' | 'value'> & { variableNames: string[]; value: string }
+export type TParsedYulExpressionStatement = Omit<TYulExpressionStatement, 'expression'> & { expression: string }
+export type TParsedYulFunctionDefinition = Omit<TYulFunctionDefinition, 'parameters' | 'returnVariables' | 'body'> & {
+  parameters: string[]
+  returnVariables: string[]
+  body: string
+}
+export type TParsedYulVariableDeclaration = Omit<TYulVariableDeclaration, 'variables' | 'value'> & { variables: string[]; value: string }
+export type TParsedYulForLoop = Omit<TYulForLoop, 'condition' | 'pre' | 'post' | 'body'> & {
+  condition: string
+  pre: string
+  post: string
+  body: string
+}
+export type TParsedYulIf = Omit<TYulIf, 'condition' | 'body'> & { condition: string; body: string }
+export type TParsedYulFunctionCall = Omit<TYulFunctionCall, 'functionName' | 'arguments'> & { functionName: string; arguments: string[] }
+
+export type TYulNodeLinkedElement = { elementSrc: string; elementNodeType: NodeType; rootNodeType: NodeType; rootSrc: string }
+
+export type TYulNodeBaseWithListIndex = TYulNodeLinkedElement & { listIndex: number }
 
 export type TYulNodeElements = Record<string, TYulNodeBaseWithListIndex>
 
