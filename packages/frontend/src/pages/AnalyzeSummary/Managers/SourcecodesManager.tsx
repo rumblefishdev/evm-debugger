3a2480c8f02b94f57fb855420abb0cbdbc354cd8
Misieq01 <38589417+Misieq01@users.noreply.github.com>
2024-03-26 09:47:38 +0100

    fix tsc

--- packages/frontend/src/pages/AnalyzeSummary/Managers/SourcecodesManager.tsx
+++ packages/frontend/src/pages/AnalyzeSummary/Managers/SourcecodesManager.tsx
@@ -1,26 +1,27 @@
 import { useSelector } from 'react-redux'
 
 import { ManagerItem } from '../../../components/ManagerItem'
+import { sourceFilesSelectors } from '../../../store/sourceFiles/sourceFiles.selectors'
 
 import { StyledContentWrapper, StyledHeading, StyledStack, StyledWrapper } from './styles'
 
 export const SourcecodesManager = () => {
   const addSourcecode = (_: string, __: string) => {}
 
-  const sourceCodesWithNames = useSelector(sourceCodesSelectors.selectAllWithContractNames)
+  const sourceFiles = useSelector(sourceFilesSelectors.selectAll)
 
   return (
     <StyledStack>
       <StyledHeading>Source Codes</StyledHeading>
       <StyledContentWrapper>
         <StyledWrapper>
-          {sourceCodesWithNames.map((item) => (
+          {sourceFiles.map((item) => (
             <ManagerItem
               key={item.address}
               address={item.address}
-              name={item.name}
-              value={item.sourceCode}
-              isFound={item.sourceCode !== null}
+              name={item.address}
+              value={JSON.stringify(item.sourceFiles)}
+              isFound={Boolean(item.sourceFiles)}
               updateItem={addSourcecode}
               contentType="solidity"
             />
